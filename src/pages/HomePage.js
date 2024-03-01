d283bc4cb7a6378763fdbd7f03e7ded003184bec
Deepak <duxedocontact@gmail.com>
2024-03-01 19:24:54 +0530

    *

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,15 +2,9 @@ import React from "react";
 import { withRouter } from "react-router";
 import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbars/Navbar";
-import { Link } from "react-router-dom";
 import styles from "./HomePage.module.css";
-import heroImg from "../assets/img/LandingPageHero.webp";
 import heroBg from "../assets/img/LandingPageHeroBgLayer.webp";
 import heroBgAlt from "../assets/img/LandingPageHeroBgLayerAlt.webp";
-import cloud1 from "../assets/img/cloud1.png";
-import cloud2 from "../assets/img/cloud2.png";
-import cloud5 from "../assets/img/cloud3.png";
-import cloud4 from "../assets/img/cloud4.png";
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -39,11 +33,7 @@ class HomePage extends React.Component {
       <div className="bg">
         <Navbar />
         <div className={styles.landing_background_verdant}>
-          <img alt="Hero" className={styles.cloud1} src={cloud1} />
-          <img alt="Hero" className={styles.cloud2} src={cloud2} />
-          <img alt="Hero" className={styles.cloud3} src={cloud5} />
-          <img alt="Hero" className={styles.cloud4} src={cloud4} />
-          <div className={styles.landing_title}>
+                    <div className={styles.landing_title}>
             <h1>Samnivesha’24</h1>
             <h2>Beyond the Blueprints</h2>
             <div>
