a1bffc73b033d97cdfaae50f2c0a4d683fd662ec
Deepak <duxedocontact@gmail.com>
2024-03-03 23:46:44 +0530

    team social link rm

--- src/components/Team/teamdata.js
+++ src/components/Team/teamdata.js
@@ -9,18 +9,13 @@ const teamdata = [
 				name: "Ayush Pal",
 				image:
 					"https://drive.google.com/uc?export=view&id=1AYNaX9esCfIYU0vbuEXKxQS5n1AwQOaX",
-				position: "",
-				fb: "https://www.facebook.com/joshika.arya",
-				ln: "https://www.linkedin.com/in/joshika/"
-			},
+						},
 			{
 				name: "Kirtan Jain",
 				image:
 					"https://drive.google.com/uc?export=view&id=1gV_2QVqzNvTj3Bh0CSzVSTOZqzcmFIln",
-				position: "",
-				fb: "https://www.facebook.com/yuvi.dhelawat",
-				ln: "https://www.linkedin.com/in/yuvi-dhelawat/"
-			}
+				
+						}
 		]
 	},
 	{
@@ -30,41 +25,41 @@ const teamdata = [
 				name: "Heet Dhorajiya",
 				image:
 					"https://drive.google.com/uc?export=view&id=140DAjWZUDpFlHwNfz_FxJvex-RrvaP_N",
-				position: "",
-				fb: "www.fb.com",
-				ln: "www.linkedin.com"
+				
+				
+				
 			}, 
       {
         name: "Rajat Kumar Behera ",
         image:
           "https://drive.google.com/uc?export=view&id=1y8AafDoLJTk3DZJqF2cr_ibmFrAGT5tX",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Kartikay ",
         image:
           "https://drive.google.com/uc?export=view&id=1U7fAnD0R_3mm8INGON0Suyj7qKX_uv2p",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Saif Rahman ",
         image:
           "https://drive.google.com/uc?export=view&id=140DAjWZUDpFlHwNfz_FxJvex-RrvaP_N",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Honey Khatri",
         image:
           "https://drive.google.com/uc?export=view&id=140DAjWZUDpFlHwNfz_FxJvex-RrvaP_N",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       }
     ]
   },
@@ -75,17 +70,17 @@ const teamdata = [
         name: "Shashikant",
         image:
           "https://deeepkk.github.io/samnivesha/src/assets/team/IMG20240204210913 - Shashikant.jpg",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Shlok Garg",
         image:
           "https://deeepkk.github.io/samnivesha/src/assets/team/Picsart_23-12-13_11-28-49-439 - shlok Garg.jpg",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       }
     ]
   },
@@ -96,17 +91,17 @@ const teamdata = [
         name: "Harsh Chandra",
         image:
           "https://deeepkk.github.io/samnivesha/src/assets/team/IMG_20240126_030043_836 - Harsh Chandra.jpg",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Shriyansh Sinha",
         image:
           "https://deeepkk.github.io/samnivesha/src/assets/team/IMG_8412 - Shriyansh Sinha.jpg",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       }
     ]
   },
@@ -117,17 +112,17 @@ const teamdata = [
         name: "Shivam Yadav",
         image:
           "https://drive.google.com/uc?export=view&id=1JFkhrC0dC1-1k_cYgvACGyFwtRojThfT",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Khushi Pathak",
         image:
           "https://drive.google.com/uc?export=view&id=1AEmVZr0vip0Xy9LDmZTQslayWihi0DXs",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       }
     ]
   },
@@ -138,49 +133,49 @@ const teamdata = [
         name: "Varesh",
         image:
           "https://drive.google.com/uc?export=view&id=g",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Dhrutisundar Sahoo",
         image:
           "https://drive.google.com/uc?export=view&id=",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Mukund Sharma",
         image:
           "https://drive.google.com/uc?export=view&id=1E3rmqFC0juv9WvOwX_eJkg41qMg20I0I",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Sahil Agrawal",
         image:
           "https://drive.google.com/uc?export=view&id=1u-_677TKn4nzVp7JlDd-e42X3ZbY3EEk",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Suryansh",
         image:
           "https://drive.google.com/uc?export=view&id=1gfXW0qaFOy-0KM3OjZKQ2Yb84kJqrg5S",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Siddhant Senapati",
         image:
           "https://drive.google.com/uc?export=view&id=1nfjXUeadXkKISnoRvr3NreddIo1sba5E",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       }
     ]
   },
@@ -191,33 +186,33 @@ const teamdata = [
         name: "Supratim Das",
         image:
           "https://drive.google.com/uc?export=view&id=1QNlSMh9w6zb9WQBYkxxTbU3R_gvqE9sU",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Naveen Kumar",
         image:
           "https://drive.google.com/uc?export=view&id=1DfpKHaC-oEpjHYxU-B10VPka-OaSN4EZ",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Harshita Uppal",
         image:
           "https://drive.google.com/uc?export=view&id=1Zgqh5usvYNmGnYKTPsmfdfNVvPxcMtGj",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Pranav Raj",
         image:
           "https://drive.google.com/uc?export=view&id=1AGMpTbO_U-82_Af5TqiN7qN5mA8zBKqg",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       }
     ]
   },
@@ -229,17 +224,17 @@ const teamdata = [
         name: "Swarnendu Maity",
         image:
           "https://deeepkk.github.io/samnivesha/src/assets/team/12_26_2023 10_42_51 AM - SWARNENDU MAITY.jpg",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       },
       {
         name: "Anjana Pathari",
         image:
           "https://deeepkk.github.io/samnivesha/src/assets/team/20230903_182101 - Anjana Pathari.jpg",
-        position: "",
-        fb: "www.fb.com",
-        ln: "www.linkedin.com"
+        
+        
+        
       }    
 ]
 
@@ -247,4 +242,3 @@ const teamdata = [
 ];
 
 export default teamdata;
-
