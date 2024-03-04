5b040e46b09ecd2d371791d278425ebe96e81ba7
Deepak <duxedocontact@gmail.com>
2024-03-04 17:26:24 +0530

    *

--- src/components/Team/teamdata.js
+++ src/components/Team/teamdata.js
@@ -9,78 +9,80 @@ const teamdata = [
 				name: "Gaurav Gupta",
 				image:
 					"https://drive.google.com/uc?export=view&id=1AYNaX9esCfIYU0vbuEXKxQS5n1AwQOaX",
-						},
-		
+			},
+
 		]
 	},
-	
-  {
-    teamname: "Media and Public Relations",
-    members: [
-      {
-        name: "Shashikant",
-        image:
-          "https://deeepkk.github.io/samnivesha/src/assets/team/IMG20240204210913 - Shashikant.jpg",
-        
-        
-        
-      },
-      {
-        name: "Shlok Garg",
-        image:
-          "https://deeepkk.github.io/samnivesha/src/assets/team/Picsart_23-12-13_11-28-49-439 - shlok Garg.jpg",
-        
-        
-        
-      }
-    ]
-  },
-  {
-    teamname: "Marketing and Sponsorship ",
-    members: [
-      {
-        name: "Harsh Chandra",
-        image:
-          "https://deeepkk.github.io/samnivesha/src/assets/team/IMG_20240126_030043_836 - Harsh Chandra.jpg",
-        
-        
-        
-      },
-      {
-        name: "Shriyansh Sinha",
-        image:
-          "https://deeepkk.github.io/samnivesha/src/assets/team/IMG_8412 - Shriyansh Sinha.jpg",
-        
-        
-        
-      }
-    ]
-  },
-  
-    
-  
-    {
-    teamname: "Events",
-    members: [
-      {
-        name: "Swarnendu Maity",
-        image:
-          "https://deeepkk.github.io/samnivesha/src/assets/team/12_26_2023 10_42_51 AM - SWARNENDU MAITY.jpg",
-        
-        
-        
-      },
-      {
-        name: "Anjana Pathari",
-        image:
-          "https://deeepkk.github.io/samnivesha/src/assets/team/20230903_182101 - Anjana Pathari.jpg",
-        
-        
-        
-      }    
-]
+	{
+		teamname: "Events",
+		members: [
+			{
+				name: "Anjana Pathari",
+				image:
+					"https://deeepkk.github.io/samnivesha/src/assets/team/20230903_182101 - Anjana Pathari.jpg",
 
-  }
+
+
+			},
+			{
+				name: "Swarnendu Maity",
+				image:
+					"https://deeepkk.github.io/samnivesha/src/assets/team/12_26_2023 10_42_51 AM - SWARNENDU MAITY.jpg",
+
+
+
+			}
+
+		]
+
+	},
+
+	{
+		teamname: "Media and Public Relations",
+		members: [
+			{
+				name: "Shashikant",
+				image:
+					"https://deeepkk.github.io/samnivesha/src/assets/team/IMG20240204210913 - Shashikant.jpg",
+
+
+
+			},
+			{
+				name: "Shlok Garg",
+				image:
+					"https://deeepkk.github.io/samnivesha/src/assets/team/Picsart_23-12-13_11-28-49-439 - shlok Garg.jpg",
+
+
+
+			}
+		]
+	},
+	{
+		teamname: "Marketing and Sponsorship ",
+		members: [
+			{
+				name: "Harsh Chandra",
+				image:
+					"https://deeepkk.github.io/samnivesha/src/assets/team/IMG_20240126_030043_836 - Harsh Chandra.jpg",
+
+
+
+			},
+			{
+				name: "Shriyansh Sinha",
+				image:
+					"https://deeepkk.github.io/samnivesha/src/assets/team/IMG_8412 - Shriyansh Sinha.jpg",
+
+
+
+			}
+		]
+	}
+
+
+
+
 ];
 
 export default teamdata;
